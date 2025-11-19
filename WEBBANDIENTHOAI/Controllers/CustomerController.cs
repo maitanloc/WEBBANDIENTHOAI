7cf0b51db87ac5c2d3932b1bee589544c303c1de
TruongBao <baotgtb01302@gmail.com>
2025-11-19 18:49:00 +0700

    load quan lý khách hàng

---
+++ WEBBANDIENTHOAI/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEBBANDIENTHOAI.Data;
+
+namespace WEBBANDIENTHOAI.Controllers
+{
+    public class CustomerController : Controller
+    {
+         private readonly AppDbContext _context;
+
+    public CustomerController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: /Customer
+    public IActionResult Index()
+    {
+        var list = _context.Customers.ToList();
+
+           
+            return View(list);
+    }
+    }
+}
