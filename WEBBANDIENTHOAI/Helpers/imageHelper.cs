7337c26a329a1e4fafb2d38c5896f4e9129d29fc
danh <phanthanhdanh7108@gmail.com>
2025-11-19 21:31:05 +0700

    chưa có j

---
+++ WEBBANDIENTHOAI/Helpers/imageHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+namespace WEBBANDIENTHOAI.Helpers
+{
+    public class imageHelper
+    {
+
+    }
+}
