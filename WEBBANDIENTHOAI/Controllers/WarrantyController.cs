c28f0456f94a588c805a2fc0d1f1f1480d00a508
maitanloc <maitanloc4012008@gmail.com>
2025-11-28 13:31:40 +0700

    cập nhật các chính sách

---
+++ WEBBANDIENTHOAI/Controllers/WarrantyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WEBBANDIENTHOAI.Controllers
+{
+    public class WarrantyController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Chính Sách Giao Hàng - FOXMOBILE";
+            return View("~/Views/ChinhSach/ChinhSachBaoHanh.cshtml");
+        }
+    }
+}
